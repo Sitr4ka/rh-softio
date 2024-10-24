554d286ee58a68e7eddfe05bf6f6d7a3271b5804
Sitr4k5 <fifasitrakarkt@gmail.com>
2024-10-24 16:22:07 +0300

    feat: add logout link

--- app/Views/_partials/header.php
+++ app/Views/_partials/header.php
@@ -9,14 +9,20 @@
     </div>
     <div class="setting-panel d-flex align-items-center justify-content-evenly">
         <div class="color-theme border border-1 text-center align-content-center pt-1">
-            <span>
+            <span id="userButton">
                 <i class="fa-solid fa-palette text-light"></i>
             </span>
         </div>
         <div class="profil border border-1 text-center align-content-center">
-            <span>
+            <span id="user_icon">
                 <i class="fa-solid fa-user-tie text-light"></i>
             </span>
+        </div>
+        <div id="userInfo" class="position-absolute d-flex flex-column d-none" style="top: 65px" >
+            <span id="username" class="ml-2">
+                <?= $user['username'] ?>
+            </span>
+            <a href="<?= base_url('auth/login') ?>">Déconnexion</a>
         </div>
     </div>
 </header
--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,9 @@ const hamburger = document.querySelector("#hamburger");
 hamburger.addEventListener('click', function(){
     document.querySelector("#sidebar").classList.toggle('expand')
 })
+
+const profil_btn = document.querySelector("#user_icon");
+
+profil_btn.addEventListener('click', function(){
+    document.querySelector("#userInfo").classList.toggle('d-none')
+})
